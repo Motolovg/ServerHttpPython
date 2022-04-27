2df835aea910587e558ddecccb0fbb2fe7552e11
motolovg <giovanediasmiranda@gmail.com>
2022-04-27 10:27:02 -0300

    teste

---
+++ server.py
@@ -0,0 +1,18 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class echoHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('content-type', 'text/html')
+        self.end_headers()
+        self.wfile.write('hello Conor'.encode())
+
+
+    def main():
+        PORT = 8000
+        server = HTTPServer(('', PORT), echoHandler)
+        print('servidor funcionando na porta %s' % PORT)
+        server.serve_forever()     
+
+    if __name__ == '__main__':
+       main() 
