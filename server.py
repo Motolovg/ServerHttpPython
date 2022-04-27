b8cb339f7af77a94f69bf0717e4a178b65d4ac37
motolovg <giovanediasmiranda@gmail.com>
2022-04-27 10:36:07 -0300

    gadsa

--- server.py
+++ server.py
@@ -1,18 +1,19 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
+
 class echoHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.send_header('content-type', 'text/html')
         self.end_headers()
-        self.wfile.write('hello Conor'.encode())
+        self.wfile.write('hello World'.encode())
 
 
-    def main():
-        PORT = 8000
-        server = HTTPServer(('', PORT), echoHandler)
-        print('servidor funcionando na porta %s' % PORT)
-        server.serve_forever()     
+def main():
+    PORT = 8000
+    server = HTTPServer(('', PORT), echoHandler)
+    print('servidor funcionando na porta %s' % PORT)
+    server.serve_forever()  
 
     if __name__ == '__main__':
        main() 
